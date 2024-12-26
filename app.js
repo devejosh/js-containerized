1a4e3dc7e5105b2e0749c14f6aa93169c5e77b85
Devendra Joshi <devendra.joshi@outlook.com>
2024-12-25 20:59:48 -0600

    update/added routes

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const port  = 3000
 
 // routes
 app.get ('/' , (req, res) =>{
-    res.send("Welcome to express inside a container. updated text")
+    res.send("Welcome, friend. this should work now, well this is perfect! Which is awesome")
 })
+
+app.get ('/about' , (req, res) =>{
+    res.send("This is the about page")
+})
+
+app.get('/admin' , (req, res) => {
+    res.send("This is the admin page")
+})
+
 
 app.listen (port, () => {
     console.log (`Server is up on port : ${port}`)
